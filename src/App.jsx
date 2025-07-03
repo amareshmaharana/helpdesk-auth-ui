b6f4a8ec8ec1ad8736b9555ed3f83d797414a79f
Amaresh Maharana <amareshcoder@gmail.com>
2025-07-03 13:38:40 +0530

    feat(routing): add home page and update auth routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ import './components/auth.css';
 import SignIn from './components/SignIn.jsx';
 import SignUp from './components/SignUp.jsx';
 import ForgotPassword from './components/ForgotPassword.jsx';
+import Home from './components/Home.jsx';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path='/' element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </Router>
